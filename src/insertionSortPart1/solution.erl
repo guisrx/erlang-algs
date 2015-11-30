7f8c1ab0b31f2ef8d4df5daccdfa8c3333961616
Selau <selau@hp.com>
2015-11-30 19:00:49 -0200

    trying insertion sort (2).

--- src/insertionSortPart1/solution.erl
+++ src/insertionSortPart1/solution.erl
@@ -1,37 +1,53 @@
 -module(solution).
 -export([main/0]).
 
-read_array(0, _D) -> [];
-read_array(Size, D) -> 
+readArray(0, _D) -> [];
+readArray(Size, D) -> 
     {ok, [X]} = io:fread("", D),
-    [X | read_array(Size - 1, D)].
+    [X | readArray(Size - 1, D)].
 
 main() ->
     {ok, [Size]} = io:fread("", "~d"),
-    Array = read_array(Size, "~d"),
-	ValueToInsert = array:get(Size - 1, Array),
-	insertOrderedValue(Array, Size - 1, ValueToInsert).
+    Array = readArray(Size, "~d"),
+	ValueToInsert = lists:nth(Size, Array),
+%	insertOrderedValue(Array, Size - 1, ValueToInsert).
+	BaseArray = lists:sublist(Array, length(Array) - 1) ++ [lists:nth(length(Array) - 1, Array)],
+	NewArray = processPosition(BaseArray, 5, 1, ValueToInsert),
+	io:format("~p~n", [NewArray]).
 
 
-insertOrderedValue(Array, 0, ValueToInsert) ->
+
+insertOrderedValue(Array, TargetIndex, Index, ValueToInsert) ->
 	array:set(Array, 0, ValueToInsert),
-	io:format("~p~n", [Array]);
-	
-insertOrderedValue(Array, Index, ValueToInsert) ->
-	PreviousValue = array:get(Index - 1, Array),
-	IsRightPlace = isRightPlace(Array, Index, PreviousValue, ValueToInsert),
-	if 
-		IsRightPlace -> true;
-		true -> insertOrderedValue(Array, Index - 1, ValueToInsert)
-	end.
-
-
-isRightPlace(Array, Index, PreviousValue, ValueToInsert) when PreviousValue <= ValueToInsert ->
-	array:set(Array, Index, ValueToInsert),
 	io:format("~p~n", [Array]),
 	true;
 
-isRightPlace(Array, Index, PreviousValue, ValueToInsert) when PreviousValue > ValueToInsert ->
-	array:set(Array, Index, PreviousValue),
-	io:format("~p~n", [Array]),
-	false.
+
+
+processPosition(Array, TargetIndex, Index, ValueToInsert) when Index == 1 ->
+	CurrentValue = lists:nth(Index, Array),
+	NextValue = lists:nth(Index + 1, Array),
+	if
+		(TargetIndex == 1) and (ValueToInsert =< NextValue) -> 
+			[ValueToInsert, CurrentValue | processPosition(Array, TargetIndex, Index + 1, ValueToInsert, CurrentValue)];
+		true -> 
+			[CurrentValue | processPosition(Array, TargetIndex, Index + 1, ValueToInsert, CurrentValue)]
+	end.
+
+
+processPosition(Array, TargetIndex, Index, ValueToInsert, PreviousValue) when Index == length(Array) ->
+	CurrentValue = lists:nth(Index, Array),
+	[CurrentValue];
+
+processPosition(Array, TargetIndex, Index, ValueToInsert, PreviousValue) when (Index < TargetIndex) or (Index > TargetIndex) ->
+	CurrentValue = lists:nth(Index, Array),
+	[CurrentValue | processPosition(Array, TargetIndex, Index + 1, ValueToInsert, CurrentValue)];
+
+processPosition(Array, TargetIndex, Index, ValueToInsert, PreviousValue) when Index == TargetIndex ->
+	CurrentValue = lists:nth(Index, Array),
+	if
+		(Index == length(Array)) and (PreviousValue > ValueToInsert) -> [CurrentValue];
+		(Index == length(Array)) and (PreviousValue =< ValueToInsert) -> [ValueToInsert];
+		true -> [CurrentValue | processPosition(Array, TargetIndex, Index + 1, ValueToInsert, CurrentValue)]
+	end;
+
